be6baaaa5c0c2c7ba3b3bebe58ed0d79a7daff8a
Jeonghwan Kim <51739985+hwan20202@users.noreply.github.com>
2026-08-12 15:27:07 +0900

    fix: grant service database access to remote root

--- modules/mysql/templates/mysql_init.sql
+++ modules/mysql/templates/mysql_init.sql
@@ -6,8 +6,12 @@ CREATE DATABASE IF NOT EXISTS fillinv_notification
     CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
 
+-- 원격 Spring 서비스가 현재 DB_USERNAME=root로 접속하므로 서비스 DB 권한을 명시적으로 부여
+GRANT ALL PRIVILEGES ON fillinv_backend.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON fillinv_notification.* TO 'root'@'%';
+
 -- Exporter 전용 유저 생성 (패스워드: exporter_password)
-CREATE USER 'exporter'@'%' IDENTIFIED BY 'exporter_password' WITH MAX_USER_CONNECTIONS 3;
+CREATE USER IF NOT EXISTS 'exporter'@'%' IDENTIFIED BY 'exporter_password' WITH MAX_USER_CONNECTIONS 3;
 
 -- 메트릭 수집에 필요한 권한만 부여
 GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
